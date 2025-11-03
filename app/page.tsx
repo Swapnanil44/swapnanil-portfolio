a344795b95c3f000da4c1e378f850fe176c73fe1
Swapnanil Majumdar <adioticswapnanil@gmail.com>
2025-11-03 21:59:04 +0530

    add intro

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import Intro from "@/components/intro";
 import Image from "next/image";
 
 export default function Home() {
   return <section className="pb-24 pt-40">
     <div className='container max-w-4xl'>
-      
+      <Intro/>
     </div>
   </section>;
 }
