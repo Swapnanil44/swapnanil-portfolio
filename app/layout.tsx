07fea76854832f55d2ade7ad3d1b54a9c9988f87
Swapnanil Majumdar <adioticswapnanil@gmail.com>
2025-11-03 22:08:41 +0530

    add footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { cn } from "@/lib/utils";
 import Providers from "@/components/providers";
 import Header from "@/components/header";
+import Footer from "@/components/footer";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 const playfair = Playfair_Display({
@@ -43,6 +44,7 @@ export default function RootLayout({
         <Providers>
           <Header />
           <main className="grow">{children}</main>
+          <Footer/>
         </Providers>
       </body>
     </html>
